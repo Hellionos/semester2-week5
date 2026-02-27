2b4623add7e08c5505eab58d3fe2c9cc02540083
Ethan West <sqqz0154@leeds.ac.uk>
2026-02-27 09:42:27 +0000

    done but want to do error handling for alphabet input

--- session_2/task1/task1.3.c
+++ session_2/task1/task1.3.c
@@ -8,7 +8,15 @@ int main(void){
  * Write a C program to read a series of integers from the user and sum them
  * until a 0 (zero) is entered. Print the sum at the end.
  */
+    int sum=0;
+    int input;
+    printf("Type some integers, if 0 is pressed the program exits\n");
+    do {
+        scanf("%d",&input);
+        sum = sum + input;
+    } while(input != 0);
 
+    printf("Sum: %d\n",sum);
 
     return 0;
 }
