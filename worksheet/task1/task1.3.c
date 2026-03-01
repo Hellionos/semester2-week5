61793894c68d45fb49235b61b4646af40d3a5c70
Ethan West <sqqz0154@leeds.ac.uk>
2026-03-01 15:02:11 +0000

    done task 1.3 ws

--- worksheet/task1/task1.3.c
+++ worksheet/task1/task1.3.c
@@ -2,7 +2,7 @@
 
 /* 
  * Task 1.3 - Practice
- * Write a program that generate two random numbers between 1 and 6 with a constraint
+ * Write a program that generates two random numbers between 1 and 6 with a constraint
  * that the sum of these two numbers cannot be 7. If the sum is 7,
  * regenerate two new random numbers. Use a do..while loop for this task.
  * The loop should terminate when a sum other than 7 is achieved and print the
@@ -14,9 +14,23 @@
 #include <time.h>
 
 int main(void) {
+
+	int min = 1;
+	int max = 6;
+	int rand1 = 0;
+	int rand2 = 0;
+	int sum = 0;
  
 	srand(time(NULL)); // Seed the random generator
 	printf("Generate two numbers between 1-6 with total cannot be 7\n");
+	do {
+		rand1 = min + rand() % (max - min +1);
+		rand2 = min + rand() % (max - min +1);
+		sum = rand1 + rand2;
+	} while (sum == 7);
     	
+	printf("Numbers rolled were %d and %d\n",rand1,rand2);
+
 	return 0;
 }
+//target = min + rand() % (max - min + 1);
