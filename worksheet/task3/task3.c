bd9c72c894a1630dba2bcfbf09be493be99df185
Ethan West <sqqz0154@leeds.ac.uk>
2026-03-01 16:28:21 +0000

    done extension task

--- worksheet/task3/task3.c
+++ worksheet/task3/task3.c
@@ -16,8 +16,62 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
  
  int main(void){
 	 
+	char input[100];
+	char input2;
+	int valid = 0;
+	int hasupper = 0;
+	int hasnumber = 0;
+	int hasspecial = 0;
+	char specialstring[] = "!@#$%^&*";
+	int exit = 1;
+
+	do {
+		do {
+			printf("\nPlease enter a password: ");
+			scanf(" %s",input);
+			hasupper = 0;
+			hasnumber = 0;
+			hasspecial = 0;
+			valid = 0;
+
+			if (strlen(input)>=8){
+				for (int i=0;i<strlen(input);i++){
+					if (isupper(input[i])){
+						hasupper = 1;
+					} 
+					if (isdigit(input[i])){
+						hasnumber = 1;
+					}
+					if (strchr(specialstring, input[i])){
+						hasspecial = 1;
+					}
+				}
+				valid = hasupper + hasnumber + hasspecial;
+				if (hasupper==0){
+					printf("Password needs a capital letter\n");
+				}
+				if (hasnumber==0){
+					printf("Password needs a number\n");
+				}
+				if (hasspecial==0){
+					printf("Password needs a special character\n");
+				}
+			} else {
+				printf("Password is too short\n");
+			}
+
+		} while (valid!=3);
+	printf("Password is valid\n");
+	printf("Do you want to validate another pasword?(y/n) ");
+	scanf(" %c",&input2);
+	if (input2 == 'y'){
+		exit = 0;
+	}
+
+	} while (exit==0);
 	 return 0;
  }
