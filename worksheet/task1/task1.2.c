15fb7cac0cc391745e38a843c26b431d515a51ee
Ethan West <sqqz0154@leeds.ac.uk>
2026-03-01 14:51:28 +0000

    done task1.2 ws

--- worksheet/task1/task1.2.c
+++ worksheet/task1/task1.2.c
@@ -11,12 +11,27 @@
 
 int main(void) {
  
-	int input1;
+	int input1 = 0;
+	int count = 0;
+	int sum = 0;
+	float average = 0;
+
 	printf("--- Average Calculator ---\n");
 	printf("Enter a -1 to exit.\n");
-	printf("Enter a postive number: ");
-	scanf("%d",&input1);
-	
+	while (input1 != -1){
+		printf("Enter a postive number: ");
+		scanf(" %d",&input1);
+		if (input1 != -1){
+			sum = sum + input1;
+			count++;
+		}
+
+	}
+	if (count != 0){
+		average = (float)sum/(float)count;
+	}
+	printf("The average of thesee numbers is %.2f\n",(float)average);
+
     	
 	return 0;
 }
